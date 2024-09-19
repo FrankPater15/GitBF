1beb0117c747c99673325a65a924595ad89b80a4
FrankPater15 <frank2018orono@gmail.com>
2024-09-18 19:14:28 -0500

    index

--- index.js
+++ index.js
@@ -4,12 +4,5 @@ const Server = require('./modules/server')
 
 const server = new Server()
 
-mongoose.connect(process.env.MONGODB_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
-.then(() => console.log('Conexión a MongoDB Atlas exitosa'))
-.catch((err) => console.error('Error al conectar a MongoDB Atlas:', err));
-
 server.listen()
 
